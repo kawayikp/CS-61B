53a3c219a349b7d9fe86f302faf4c5c1f38df4e0
June <liuyue.kp@gmail.com>
2016-04-23 07:55:43 -0700

    no message

---
+++ hw5/list/InvalidNodeException.java
@@ -0,0 +1,17 @@
+package java61b.hw5.list;
+
+/* InvalidNodeException.java */
+
+/**
+ *  Implements an Exception that signals an attempt to use an invalid ListNode.
+ */
+
+public class InvalidNodeException extends Exception {
+  protected InvalidNodeException() {
+    super();
+  }
+
+  protected InvalidNodeException(String s) {
+    super(s);
+  }
+}
