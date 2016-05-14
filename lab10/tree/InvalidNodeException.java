1cc3eb63a30ab09c3b195c3c554ad2271f3f06d4
June <liuyue.kp@gmail.com>
2016-05-14 13:03:59 -0700

    no message

---
+++ lab10/tree/InvalidNodeException.java
@@ -0,0 +1,16 @@
+package java61b.lab10.tree;
+
+
+/**
+ *  Implements an Exception that signals an attempt to use an invalid ListNode.
+ */
+
+public class InvalidNodeException extends Exception {
+  protected InvalidNodeException() {
+    super();
+  }
+
+  protected InvalidNodeException(String s) {
+    super(s);
+  }
+}
